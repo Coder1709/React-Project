1820b32743b262bc64d198f2e6e921294a7c2a26
Arpit Pathak <110691809+Coder1709@users.noreply.github.com>
2023-02-14 18:48:08 +0530

    Project : Books Name Storing App
    Its the first version of the project which stores which books you want to read, it uses react as the main technology Visual studio code is also used. You need to install node module (npx create-react-app books) and change the existing files it it except node_module for this project to work

---
+++ Project_6 (books)/src/components/BookShow.js
@@ -0,0 +1,43 @@
+import {useState} from 'react'
+import BookEdit from './BookEdit';
+
+function BookShow({ book, onDelete ,onEdit}) {
+
+  const [showEdit , setShowEdit] = useState(false);
+
+
+  const handleDeleteClick = () => {
+    onDelete(book.id);
+  };
+  const handleEditClick = () => {
+    setShowEdit(!showEdit);
+  }
+
+  const handleSubit = (id ,newTitle) => {
+    setShowEdit(false);
+    onEdit(id , newTitle);
+  }
+
+  let content = <h3>{book.title}</h3>
+
+  if (showEdit) {
+    content = <BookEdit onSubmit = {handleSubit} book = {book}/>
+  }
+  return (
+    <div className="book-show">
+      <img src = { `https://picsum.photos/seed/${book.id}/300/200` }alt = "images" />
+      <div>{content}</div>
+  
+      <div className="actions">
+        <button className='edit' onClick = {handleEditClick}>
+          Edit
+        </button>
+        <button className="delete" onClick={handleDeleteClick}>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default BookShow;
