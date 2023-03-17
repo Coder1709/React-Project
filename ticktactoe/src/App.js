00d49f057d924877eef19193d11f0f40f7d1b506
Arpit Pathak <110691809+Coder1709@users.noreply.github.com>
2023-03-17 19:32:13 +0530

    Add files via upload

---
+++ ticktactoe/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Board from './ticktactoegame/Board';
+
+function App() {
+  return (
+    <div className="App">
+      <Board />
+      
+    </div>
+  );
+}
+
+export default App;
